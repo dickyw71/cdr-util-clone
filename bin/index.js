922153313aef7d7934f56204f1dd80af43c3ca63
Richard Wheatley <dickyw71@users.noreply.github.com>
2018-07-25 21:52:24 +0100

    Replaced greeting code with processing of CL args.

--- bin/index.js
+++ bin/index.js
@@ -1,40 +1,31 @@
 #!/usr/bin/env node
 
-const colors = require('colors');
-const greet = require("../lib/greet");
+const fs = require('fs');
 
 // get arguments after first two elements in process.argv
 var arguments = process.argv.splice(2);
 
-// check if user want language specific greeting
-// default value of language is `null`
-var lang = null;
+var filepath = arguments[0];
+var infile  = arguments[1];
 
-// check if first argument is `--lang`
-if (arguments[0] == '--lang') {
-    // set second argument as language.
-    lang = arguments[1];
-}
+console.log(`filepath: ${filepath}`);
+console.log(`inputFilename: ${infile}`);
+
+// check that the filepath and input file can be r/w accessed
+var fileAccessOk = false;
+fs.access(`${filepath}`, fs.constants.R_OK | fs.constants.W_OK, (err) => {
+    console.log(err ? 'no access!' : 'can read/write');
+    err ? fileAccessOk = false : fileAccessOk = true; 
+    if (!fileAccessOk) {
+        console.log(`No access to specified filepath, exiting!`);
+        process.exit(1);
+    }
+  });
 
 
-// if `lang` is empty, then show random greeting
-if (lang) {
-    // print random greeting
-    console.log(
-        // wraps text with rainbow color formatting
-        colors.rainbow(
-            // returns the greeting text with specified language
-            greet.greet(lang)
-        )
-    );
-}
-else {
-    // print random greeting
-    console.log(
-        // wraps text with rainbow color formatting
-        colors.rainbow(
-            // returns the random greeting text
-            greet.greetRandom()
-        )
-    );
-}
+var verboseOn = (arguments[2] == '-v' ? true : false);
+var outfile = arguments[3] || null;
+
+
+console.log(`Verbose: ${verboseOn ? 'On' : 'Off'}`)
+console.log(`outFilename prefix: ${outfile}_`);
