21dd78b6c91663be8444d70ce969f08875275c29
dickyw71 <dickyw71@users.noreply.github.com>
2018-07-28 14:54:38 +0100

    Writing to CDRlog file and reading the input file.

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const readline = require('readline');
 
 // get arguments after first two elements in process.argv
 var arguments = process.argv.splice(2);
@@ -13,7 +14,7 @@ console.log(`inputFilename: ${infile}`);
 
 // check that the filepath and input file can be r/w accessed
 var fileAccessOk = false;
-fs.access(`${filepath}`, fs.constants.R_OK | fs.constants.W_OK, (err) => {
+fs.access(`${filepath}/${infile}`, fs.constants.R_OK | fs.constants.W_OK, (err) => {
     console.log(err ? 'no access!' : 'can read/write');
     err ? fileAccessOk = false : fileAccessOk = true; 
     if (!fileAccessOk) {
@@ -28,9 +29,30 @@ var outfile = arguments[3] || null;
 
 
 console.log(`Verbose: ${verboseOn ? 'On' : 'Off'}`)
-console.log(`outFilename prefix: ${outfile}_`);
+console.log(`outFilename prefix: ${outfile ? outfile + '_' : 'N/A'}`);
+console.log(`CDRlog filename CDRlog_${infile}.log`);
 
-// read the infile
+// write the CDRlog file to filepath with start of request
+const logFileWS = fs.createWriteStream(`${filepath}/CDRlog_${infile}.log`)
+logFileWS.write(`CDRutil started ${new Date(Date.now()).toLocaleString()}\n`)
+
+// read the infile line by line
+const infileRS = fs.createReadStream(`${filepath}/${infile}`)
+infileRS.setEncoding('utf8')
+
+const rl = readline.createInterface(infileRS);
+
+rl.on('line', (line) => {
+    // write the log file with name of each sensor requested
+    logFileWS.write(`Barcode requested: ${line}\n`)
+
+})
+
+rl.on('close', () => {
+    logFileWS.end()
+})
+
+
 
 // query the DB
 
