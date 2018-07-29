0c084e411db002f79efad5a435ee3315a516f43a
Richard Wheatley <dickyw71@users.noreply.github.com>
2018-07-29 21:33:45 +0100

    Added data access and file writing.

--- bin/index.js
+++ bin/index.js
@@ -2,6 +2,7 @@
 
 const fs = require('fs');
 const readline = require('readline');
+const da = require('../lib/data-access.js');
 
 // get arguments after first two elements in process.argv
 var arguments = process.argv.splice(2);
@@ -11,6 +12,12 @@ const infile  = arguments[1];
 
 const verboseOn = (arguments[2] == '-v' ? true : false);
 const outfile = arguments[3] || null;
+
+// log file counters
+let numOfBarcodesFound = 0;
+let numOfBarcodesNotFound = 0;
+let numOfBarcodeErrors = 0;
+
 
 console.log(`filepath: ${filepath}`);
 console.log(`inputFilename: ${infile}`);
@@ -28,31 +35,62 @@ fs.access(`${filepath}/${infile}`, fs.constants.R_OK | fs.constants.W_OK, (err) 
         process.exit(1);
     }
     else {
-       // write the CDRlog file to filepath with start of request
-       const logFileWS = fs.createWriteStream(`${filepath}/CDRlog_${infile}.log`)
-       logFileWS.write(`CDRutil started ${new Date(Date.now()).toLocaleString()}\n`)
+        // create the outfile
+        const outfileWS = fs.createWriteStream(`${filepath}/${infile}.csv`) 
 
-       // read the infile line by line
-       const infileRS = fs.createReadStream(`${filepath}/${infile}`)
-       infileRS.setEncoding('utf8')
+        // write the CDRlog file to filepath with start of request
+        const logFileWS = fs.createWriteStream(`${filepath}/CDRlog_${infile}.log`)
+        logFileWS.write(`CDRutil started ${new Date(Date.now()).toLocaleString()}\n`)
 
-       const rl = readline.createInterface(infileRS);
+        // if(!da.openDbConnection()) {    // Failed openning connection to database 
+        //     console.log('failed opening DB connection')
+        // }
+        // else {
+            da.openDbConnection();
+            // read the infile line by line
+            const infileRS = fs.createReadStream(`${filepath}/${infile}`)
+            infileRS.setEncoding('utf8')
+            const rl = readline.createInterface(infileRS);
 
-       rl.on('line', (line) => {
-           // write the log file with name of each sensor requested
-           logFileWS.write(`Barcode requested: ${line}\n`)
+            //  readline 'line' event handler - fired for each line read from the infile 
+            rl.on('line', (barcode) => {
+                console.log(barcode)
+                // write the log file with name of each sensor requested
+                logFileWS.write(`Barcode requested: ${barcode}\n`)
 
-           // query the DB
+                // query the DB
+                const resultRow = da.getSensor(barcode);
 
-           // write the CDR_log file with status of each sensor query
+                 // write the CDR_log file with status of each sensor query
+                if(resultRow === null) {    // error getting barcode
+                    numOfBarcodeErrors++
+                    logFileWS.write(`Error returned for barcode:${barcode}.`)
+                }
+                if(resultRow.length === 0) { //  barcode not found
+                    numOfBarcodesNotFound++
+                    logFileWS.write(`Barcode:${barcode} was not found.`)
+                }
+                // write the query results to the output file or files if an outfile is non-null
+                if(resultRow.length === 1) { // barcode found
+                    numOfBarcodesFound++
+                    logFileWS.write(`Barcode:${barcode} was found OK.`)
+                    outfileWS.write(`${resultRow[0].toString()}\n`)
+                }
+            })
 
-           // write the query results to the output file or files if an outfile is non-null
-
-       })
-
-       rl.on('close', () => {
-           logFileWS.end()
-       })
+            //  readline 'close' event handler - fired when the end of the read stream is reached
+            rl.on('close', () => {
+                console.log('Closing everything')
+                da.closeDbConnection();
+                logFileWS.write(`***********************************************\n
+                                Number of barcodes found: ${numOfBarcodesFound}\n\n
+                                Number of barcodes not found: ${numOfBarcodesNotFound}\n\n
+                                Number of barcodes rejected: ${numOfBarcodeErrors}`)
+                logFileWS.write(`CDRutil finished ${new Date(Date.now()).toLocaleString()}\n`)                
+                logFileWS.end()
+                outfileWS.end()   
+            })
+        // }
     }
   });
 
