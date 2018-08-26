7db6a670f705ce8354b4a7960710e887b997a18d
Richard Wheatley <dickyw71@users.noreply.github.com>
2018-08-26 11:13:37 +0100

    Main with Promises

--- bin/main-promises.js
+++ bin/main-promises.js
@@ -1,14 +1,33 @@
 #!/usr/bin/env node
 
 const oracledb = require('oracledb');
-const input = require('../lib/read-infile.js');
+const input = require('../lib/read-infile-promises.js');
 const dbconfig = require('../lib/db-config.js');
-const sensor = require('../lib/cert-caldue.js');
+const sensor = require('../lib/cert-caldue-promises.js');
 
 let file = `/Users/richardwheatley/Developer/cdr-util-clone/test/dmc`
+let sensors;
 
-
-// input.getSensors(file)
-//     .then(function(sensors) {
-
-//     })
+//  Using Promises
+// Create a default database connection pool 
+oracledb
+    .createPool(dbconfig)
+    .then(function() {
+        input.readLines(file)
+            .then(function(s) {
+                sensors = s
+                console.log(sensors)
+            })
+            .catch(function (error) {
+                console.log(error)
+            })
+    })
+    .then(function() {
+        return sensor.getCalCertAndDueDate('TZ000005');
+    })
+    .then(function(result) {
+        console.log(result[0]);
+    })
+    .catch(function(err) {
+        console.log(err);
+    });
